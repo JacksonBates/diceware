e159f2761ad139f55c0dcc4e215236165614d6cd
Jackson Bates <malgalin@gmail.com>
2018-12-24 14:56:55 +1100

    Updates

---
+++ precache-manifest.b2fc23f02b22015651bb687fd2728156.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb2f03f65d696478fbce",
+    "url": "/diceware/static/js/runtime~main.bb2f03f6.js"
+  },
+  {
+    "revision": "73685c64ac70f23f6b0e",
+    "url": "/diceware/static/js/main.73685c64.chunk.js"
+  },
+  {
+    "revision": "23b00d73a33e8f2611fa",
+    "url": "/diceware/static/js/1.23b00d73.chunk.js"
+  },
+  {
+    "revision": "73685c64ac70f23f6b0e",
+    "url": "/diceware/static/css/main.ef2cabed.chunk.css"
+  },
+  {
+    "revision": "23b00d73a33e8f2611fa",
+    "url": "/diceware/static/css/1.d1d4bd3c.chunk.css"
+  },
+  {
+    "revision": "4929b2cf1bb2cda33a068f974840cdda",
+    "url": "/diceware/index.html"
+  }
+];
